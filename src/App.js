29ec74b80c68b2d52a87f923baff73e12954e6b7
itsjustxtian <ocon.christiane@gmail.com>
2023-10-27 13:14:21 +0800

    Forda tutorial.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import SignUp from './components/auth/SignUp';
 import AuthDetails from './components/auth/AuthDetails';
 import './fonts.css';
 import Sidebar from './components/sidebar'
+/*import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';*/
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import inbox from './components/inbox'
+import Dashboard from './components/Dashboard'
+import usermanagement from './components/usermanagement'
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar/>
-      {/*<SignIn/>
-      <SignUp/>
-      <AuthDetails/>*/}
-    </div>
+    <Router>
+      <div className="App">
+        <div className='container'>
+        <Sidebar/>
+        <Dashboard></Dashboard> {/* Change this line to your component */}
+        </div>
+        
+        <Routes>
+          <Route path='/dashboard' element={Dashboard}/>
+          <Route path='/inbox' element={inbox}/>
+          <Route path='/usermanagement' element={usermanagement}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
