8a6bc086f5c50f0ea772779f70cc4ddd179621ce
itsjustxtian <ocon.christiane@gmail.com>
2023-12-02 23:31:29 +0800

    Updated App.css, created AddApplications form

--- src/components/UserMng/AddApplications.jsx
+++ src/components/UserMng/AddApplications.jsx
@@ -2,8 +2,59 @@ import React from 'react'
 
 const AddApplications = () => {
   return (
-    <div>
-      
+    <div className='add-application'>
+
+      <div id='new-line'>
+        <label>
+          Name:
+        </label>
+        <input/>
+      </div>
+
+      <div id='new-line'>
+        <label>
+          Team Leader:
+        </label>
+        <input/>
+      </div>
+
+      <div id='new-line'>
+        <label>
+          Assigned QA:
+        </label>
+        <input/>
+      </div>
+
+      <div id='new-line'>
+        <label>
+          Members:
+        </label>
+        <input/>
+      </div>
+
+      <div className='space'/>
+
+      <div id='new-line'>
+        <label>
+          Description:
+        </label>
+      </div>
+
+      <div id='new-line'>
+        <textarea
+          cols={30}
+          rows={10}/>
+      </div>
+
+      <div className='formbuttons'>
+          <button className='submit'>
+            Submit
+          </button>
+          <button className='cancel' id='text'>
+              <div id='text'> Cancel </div>
+            </button>
+        </div> 
+
     </div>
   )
 }
