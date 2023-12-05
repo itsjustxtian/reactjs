aa6d07379b8d76c6004ce633ac235c03923296c1
Jy-rehh <jyreh28@gmail.com>
2023-12-06 00:14:20 +0800

    Sugdan Design sa View profile, wagtanga lang if nasayop ko

--- src/components/UserMng/ViewProfile.jsx
+++ src/components/UserMng/ViewProfile.jsx
@@ -1,8 +1,61 @@
+import { Tab } from '@mui/material'
 import React from 'react'
 
 const ViewProfile = () => {
   return (
-    <div>
+    <div className='viewProfile'>
+          Connect nalang nis database(ata)
+        <div className='box'>
+            <div className="rectangle" />
+          </div>
+          
+          <div className='appLabel'>
+            <div id= 'new-line1'> 
+              <label>
+              NAME
+              </label>
+            </div>
+
+            <div id= 'new-line'> 
+              <label>
+              COMPANY ID
+              </label>
+            </div>
+
+            <div id= 'new-line'> 
+              <label>
+              EMAIL
+              </label>
+            </div>
+
+            <div id= 'new-line2'> 
+              <label>
+              Status
+              </label>
+            </div>
+
+            <div id= 'new-line3'> 
+              <label>
+              Assignments:
+              </label>
+            </div>
+
+            <div id= 'new-line4'> 
+              <label>
+              Application 1 <Tab></Tab> <Tab></Tab>  Team Leader 1
+              </label>
+            </div>
+
+            <div id= 'new-line4'> 
+              <label>
+              Application 2 <Tab></Tab> <Tab></Tab>  Team Leader 2
+              </label>
+            </div>
+
+            <br>
+            </br>
+              AMBOT SAKTO BA
+          </div>
       
     </div>
   )
